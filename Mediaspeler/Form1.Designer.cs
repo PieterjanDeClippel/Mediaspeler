9c36b4f73547f5aab33ae36962a3c31689437bb6
Piejie <pieterjandeclippel@msn.com>
2017-02-15 20:18:32 +0100

    MP3-player
    Play MP3-files in Visual Studio

---
+++ Mediaspeler/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Mediaspeler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.lblBestand = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.nudSPP = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSPP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecteer bestand...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(12, 227);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(100, 50);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "►";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(250, 227);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 50);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "▉";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(132, 227);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(100, 50);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "p";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // lblBestand
+            // 
+            this.lblBestand.AutoSize = true;
+            this.lblBestand.Location = new System.Drawing.Point(168, 18);
+            this.lblBestand.Name = "lblBestand";
+            this.lblBestand.Size = new System.Drawing.Size(0, 13);
+            this.lblBestand.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(396, 149);
+            this.panel1.TabIndex = 5;
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // nudSPP
+            // 
+            this.nudSPP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudSPP.Location = new System.Drawing.Point(132, 198);
+            this.nudSPP.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudSPP.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudSPP.Name = "nudSPP";
+            this.nudSPP.Size = new System.Drawing.Size(100, 20);
+            this.nudSPP.TabIndex = 0;
+            this.nudSPP.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.nudSPP.ValueChanged += new System.EventHandler(this.nudSPP_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Samples per Pixel:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 292);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudSPP);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblBestand);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudSPP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label lblBestand;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown nudSPP;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Mediaspeler/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.Wave;
+using System.IO;
+
+namespace Mediaspeler
+{
+    public partial class Form1 : Form
+    {
+        IWavePlayer player;
+        AudioFileReader reader;
+        NAudio.Gui.WaveViewer viewer = new NAudio.Gui.WaveViewer();
+        public Form1()
+        {
+            InitializeComponent();
+            viewer.Dock = DockStyle.Fill;
+            viewer.BackColor = Color.White;
+            viewer.SamplesPerPixel = 10;
+            panel1.Controls.Add(viewer);
+            viewer.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Muziek|*.mp3;*.wav";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string tmpname = Path.GetTempFileName();
+                if (ofd.FileName.Split('.').Last() == "mp3")
+                {
+                    using (Mp3FileReader reader = new Mp3FileReader(ofd.FileName))
+                    {
+                        WaveFileWriter.CreateWaveFile(tmpname, reader);
+                    }
+                }
+                lblBestand.Text = ofd.FileName;
+                viewer.WaveStream = new NAudio.Wave.WaveFileReader(tmpname);                
+                btnPlay.Enabled = true;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            btnPlay.Enabled = true;
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
+        }
+
+        void player_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            btnPlay.Enabled = true;
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if(player == null)
+            {
+                player = new WaveOut();
+                reader = new AudioFileReader(lblBestand.Text);
+                player.Init(reader);
+                player.PlaybackStopped += player_PlaybackStopped;
+                player.Play();
+            }
+            else if (player.PlaybackState == PlaybackState.Paused)
+                player.Play();
+            else if (player.PlaybackState == PlaybackState.Stopped)
+            {
+                player = new WaveOut();
+                reader = new AudioFileReader(lblBestand.Text);
+                player.Init(reader);
+                player.Play();
+            }
+            btnPlay.Enabled = false;
+            btnPause.Enabled = true;
+            btnStop.Enabled = true;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            player.Pause();
+            btnPlay.Enabled = true;
+            btnPause.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            viewer.Invalidate();
+        }
+
+        private void nudSPP_ValueChanged(object sender, EventArgs e)
+        {
+            if (viewer != null)
+                viewer.SamplesPerPixel = (int)nudSPP.Value;
+        }
+    }
+}
